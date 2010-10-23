99c330720d28630c02a7d850a07acc7eb1caa2ce
Jarno Keskikangas <jarno.keskikangas@leonidasoy.fi>
2010-10-23 20:02:00 +0300

    resolved sftp problem with deployment scripts

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,11 @@
 set :stages, %w(staging production)
 set :default_stage, "staging"
 
-set :copy_compression, :zip
+#set :copy_compression, :zip
 
 require 'capistrano/ext/multistage'
 require 'config/capistrano_database_yml'
+require 'bundler/capistrano'
 
 set :app_env, "production"
 set :rails_env, "production"
@@ -36,11 +37,10 @@ ssh_options[:forward_agent] = true
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 set :scm, :git
-# see a full list by running "gem contents capistrano | grep 'scm/'"
 
-#after "deploy:symlink" do
-#  run "ln -nfs #{shared_path}/reports #{current_path}/public/"
-#end
+after "deploy:symlink" do
+  run "ln -nfs #{shared_path}/reports #{current_path}/public/"
+end
 
 namespace :deploy do
   desc "Restart Application"
@@ -55,30 +55,3 @@ namespace :deploy do
 
 end
 
-namespace :bundler do
-  task :create_symlink, :roles => :app do
-    shared_dir = File.join(shared_path, 'bundle')
-    release_dir = File.join(release_path, '.bundle')
-    run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
-  end
-
-  task :install, :roles => :app do
-    run "cd #{release_path} && bundle install"
-
-    on_rollback do
-      if previous_release
-        run "cd #{previous_release} && bundle install"
-      else
-        logger.important "no previous release to rollback to, rollback of bundler:install skipped"
-      end
-    end
-  end
-
-  task :bundle_new_release, :roles => :db do
-    bundler.create_symlink
-    bundler.install
-  end
-end
-
-after "deploy:rollback:revision", "bundler:install"
-after "deploy:update_code", "bundler:bundle_new_release"
--- config/deploy/capistrano_database_yml.rb
+++ config/deploy/capistrano_database_yml.rb
@@ -127,7 +127,7 @@ Capistrano::Configuration.instance.load do
           database: #{shared_path}/db/test.sqlite3
           <<: *base
         production:
-          database: #{shared_path}/db/production.sqlite3
+          database: #{Capistrano::CLI.ui.ask("Enter MySQL database password: ")} 
           <<: *base
         EOF
 
@@ -138,16 +138,7 @@ Capistrano::Configuration.instance.load do
 
         run "mkdir -p #{shared_path}/db" 
         run "mkdir -p #{shared_path}/config" 
-
-        config_path = ""
-        run "cd #{shared_path}/config/ && pwd" do |ch, stream, data|
-          if stream == :out
-            config_path += data
-          end
-        end
-
-        config_path.chomp!
-        put config.result(binding), "#{config_path}/database.yml"
+        put config.result(binding), "#{shared_path}/config/database.yml"
       end
 
       desc <<-DESC
