35379256bb3db5e9e9c0f55618dec3a4af442fe2
Tommi Reinikainen <tommi.reinikainen@gmail.com>
2010-10-30 15:21:42 +0300

    added seeding script for 1000 test reports to staging

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,42 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-if Rails.env == "development":
-  User.create! :password => 'testpass', :email => 'test@leonidasoy.fi', :name => "Jean-Claude Van Damme"
+if Rails.env == "development" or Rails.env == "staging"
+  User.create! :password => 'testpass',
+               :email => 'test@leonidasoy.fi',
+               :name => "Jean-Claude Van Damme" unless User.exists? :email => 'test@leonidasoy.fi'
 end
 
+if Rails.env == "staging" and MeegoTestSession.count < 10000 # ensure there's always 10000 reports on database
+  testuser = User.find_by_email("test@leonidasoy.fi")
+  fpath = File.join(Rails.root, "features", "resources", "sample.csv")
+  tmp_file = Tempfile.new("csvfile") do |tempfile|
+    File.open(fpath, "r") do |csv_file|
+      tempfile.write(csv_file.read)
+    end      
+  end
+
+  File.rename(tmp_file.path, tmp_file.path + ".csv")
+  tmp_file = File.open(tmp_file.path + ".csv")
+
+  10000.times do
+    session = MeegoTestSession.new(
+      "build_txt" => "",
+      "qa_summary_txt" => "",
+      "uploaded_files" => [tmp_file.path],
+      "testtype" => "Acceptance",
+      "hwproduct" => "N900",
+      "environment_txt" => "",
+      "issue_summary_txt" => "",
+      "target" => "Core",
+      "objective_txt" => ""
+    )
+    session.generate_defaults!
+    session.tested_at = Time.now
+    session.author = testuser
+    session.editor = testuser
+    session.published = true
+    session.save
+  end
+
+end
