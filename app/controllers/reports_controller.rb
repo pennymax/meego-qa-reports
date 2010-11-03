fdae83439379a53cb66743fe8ec7b03b26d160a2
Edvard Majakari <edvard.majakari@leonidasoy.fi>
2010-11-04 01:27:17 +0200

    fixed nil.to_i bug from many methods

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -26,86 +26,85 @@ require 'drag_n_drop_uploaded_file'
 
 class ReportsController < ApplicationController
   before_filter :authenticate_user!, :only => ["upload", "upload_form", "edit", "delete", "update", "update_txt",
-                                               "update_title", "update_case_comment", "update_case_result"]
+      "update_title", "update_case_comment", "update_case_result"]
   #caches_page :print
   #caches_page :index, :upload_form, :email, :filtered_list
   #caches_page :view, :if => proc {|c|!c.just_published?}
   caches_action :fetch_bugzilla_data,
-                :cache_path => Proc.new { |controller| controller.params },
-                :expires_in => 1.hour
+      :cache_path => Proc.new { |controller| controller.params },
+      :expires_in => 1.hour
 
   def preview
-    @preview_id = session[:preview_id] || params[:id].to_i
+    @preview_id = session[:preview_id] || params[:id]
     @editing = true
     @wizard = true
-    
-    unless @preview_id
-      redirect_to :action => :upload_form
-      return
-    end
-    
-    @test_session = MeegoTestSession.find(@preview_id)
-    @report = @test_session
-    @no_upload_link = true
 
-    render :layout => "report"
+    if @preview_id
+      @test_session = MeegoTestSession.find(@preview_id)
+      @report = @test_session
+      @no_upload_link = true
+
+      render :layout => "report"
+    else
+      redirect_to :action => :upload_form
+    end
   end
-  
+
   def update_txt
     @preview_id = params[:id].to_i
     @test_session = MeegoTestSession.find(@preview_id)
-    
+
     field = params[:meego_test_session]
     field = field.keys()[0]
-    @test_session.update_attribute(field, params[:meego_test_session][field]);
+    @test_session.update_attribute(field, params[:meego_test_session][field])
     @test_session.updated_by(current_user)
     expire_caches_for(@test_session)
-    
+
     sym = field.sub("_txt", "_html").to_sym
-    
-    render :text => @test_session.send(sym) 
+
+    render :text => @test_session.send(sym)
   end
-  
+
   def update_title
     @preview_id = params[:id].to_i
     @test_session = MeegoTestSession.find(@preview_id)
-    
+
     field = params[:meego_test_session]
     field = field.keys()[0]
-    @test_session.update_attribute(field, params[:meego_test_session][field]);
+    @test_session.update_attribute(field, params[:meego_test_session][field])
     @test_session.updated_by(current_user)
     expire_caches_for(@test_session)
     expire_index_for(@test_session)
 
-    render :text => "OK" 
+    render :text => "OK"
   end
-  
+
   def update_case_comment
-  	case_id = params[:id]
-  	comment = params[:comment]
-  	testcase = MeegoTestCase.find(case_id)
-  	testcase.update_attribute(:comment, comment)
+    case_id = params[:id]
+    comment = params[:comment]
+    testcase = MeegoTestCase.find(case_id)
+    testcase.update_attribute(:comment, comment)
 
     test_session = testcase.meego_test_session
     test_session.updated_by(current_user)
     expire_caches_for(test_session)
 
-  	render :text => "OK"
+    render :text => "OK"
   end
-  
+
   def update_case_result
-  	case_id = params[:id]
-  	result = params[:result]
-  	testcase = MeegoTestCase.find(case_id)
-  	testcase.update_attribute(:result, result.to_i)
-    
+    case_id = params[:id]
+    result = params[:result]
+    testcase = MeegoTestCase.find(case_id)
+    testcase.update_attribute(:result, result.to_i)
+
     test_session = testcase.meego_test_session
     test_session.updated_by(current_user)
     expire_caches_for(test_session, true)
-    
+
     render :text => "OK"
   end
-  
+
   def publish
     report_id = params[:report_id]
     test_session = MeegoTestSession.find(report_id)
@@ -113,80 +112,78 @@ class ReportsController < ApplicationController
 
     expire_caches_for(test_session, true)
     expire_index_for(test_session)
-    
+
     redirect_to :action => 'view',
-                :id => report_id,
-                :release_version => @selected_release_version,
-                :target => test_session.target,
-                :testtype => test_session.testtype,
-                :hwproduct => test_session.hwproduct
+        :id => report_id,
+        :release_version => @selected_release_version,
+        :target => test_session.target,
+        :testtype => test_session.testtype,
+        :hwproduct => test_session.hwproduct
   end
-  
+
   def view
-    @report_id = params[:id].try(:to_i)
-    unless @report_id
-      redirect_to :action => :index
-      return
-    end
-    preview_id = session[:preview_id]
-    if preview_id == @report_id
-      session[:preview_id] = nil
-      @published = true
+    if @report_id = params[:id].try(:to_i)
+      preview_id = session[:preview_id]
+
+      if preview_id == @report_id
+        session[:preview_id] = nil
+        @published = true
+      else
+        @published = false
+      end
+
+      @test_session = MeegoTestSession.find(@report_id)
+      @target = @test_session.target
+      @testtype = @test_session.testtype
+      @hwproduct = @test_session.hwproduct
+
+      @report = @test_session
+      @editing = false
+      @wizard = false
+
+      render :layout => "report"
     else
-      @published = false
-    end
-    @test_session = MeegoTestSession.find(@report_id)
-    @target = @test_session.target
-    @testtype = @test_session.testtype
-    @hwproduct = @test_session.hwproduct
-    
-    @report = @test_session
-    @editing = false
-    @wizard = false
-    
-    render :layout => "report"
-  end
-  
-  def print
-    @report_id = params[:id].to_i
-    unless @report_id
       redirect_to :action => :index
-      return
     end
-    @test_session = MeegoTestSession.find(@report_id)
-    
-    @report = @test_session
-    @editing = false
-    @wizard = false
-    @email = true
-    
-    render :layout => "report"
   end
-  
+
+  def print
+    if @report_id = params[:id].try(:to_i)
+      @test_session = MeegoTestSession.find(@report_id)
+
+      @report = @test_session
+      @editing = false
+      @wizard = false
+      @email = true
+
+      render :layout => "report"
+    else
+      redirect_to :action => :index
+    end
+  end
+
   def edit
-    id = params[:id].to_i
     @editing = true
     @wizard = false
-    
-    unless id
+
+    if id = params[:id].try(:to_i)
+      @test_session = MeegoTestSession.find(id)
+      @report = @test_session
+      @no_upload_link = true
+
+      render :layout => "report"
+    else
       redirect_to :action => :index
-      return
     end
-    
-    @test_session = MeegoTestSession.find(id)
-    @report = @test_session
-    @no_upload_link = true
-    
-    render :layout => "report"
   end
-  
+
   def fetch_bugzilla_data
     ids = params[:bugids]
     searchUrl = "http://bugs.meego.com/buglist.cgi?bugidtype=include&columnlist=short_desc%2Cbug_status%2Cresolution&query_format=advanced&ctype=csv&bug_id=" + ids.join(',')
     data = open(searchUrl)
     render :text => data.read(), :content_type => "text/csv"
   end
-  
+
   def delete
     id = params[:id]
 
@@ -194,17 +191,17 @@ class ReportsController < ApplicationController
 
     expire_caches_for(test_session, true)
     expire_index_for(test_session)
-    
+
     test_session.destroy
 
     redirect_to :controller => :index, :action => :index
   end
-  
-protected
+
+  protected
 
   def just_published?
     @published
-  end  
+  end
 
   def expire_index_for(test_session)
     expire_page :controller => 'index', :action => :index
