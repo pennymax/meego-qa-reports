f51c0600ed6368f49b61824d30a0c4e708aaacf3
Edvard Majakari <edvard.majakari@leonidasoy.fi>
2010-11-04 01:40:07 +0200

    initial helper specs

--- spec/helpers/meego_test_case_helper_spec.rb
+++ spec/helpers/meego_test_case_helper_spec.rb
@@ -10,6 +10,20 @@ require 'spec_helper'
 #     end
 #   end
 # end
+
 describe MeegoTestCaseHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should give proper html for test case result" do
+    [
+        [1, "Pass"],
+        [-1, 'Fail']
+    ].each do |result_code, str|
+      t_case = MeegoTestCase.new(:result => result_code)
+      result_html(t_case).should == str
+    end
+  end
+
+  it "should give proper, escaped html for test case comment" do
+    t_case = MeegoTestCase.new(:comment => '<script>mah buckit</script>')
+    comment_html(t_case).should == '&lt;script&gt;mah buckit&lt;/script&gt;<br/>'
+  end
 end
