31898fe2e2ff55eb28ddeb50e3336f1acc8ce711
Sami Hangaslammi <sami.hangaslammi@leonidasoy.fi>
2010-11-06 15:24:54 +0200

    updated prev/next session logic

--- app/models/meego_test_session.rb
+++ app/models/meego_test_session.rb
@@ -153,18 +153,18 @@ class MeegoTestSession < ActiveRecord::Base
   # Test session navigation                     #
   ###############################################
   def prev_session
-    time = created_at || Time.now
+    time = tested_at || Time.now
     MeegoTestSession.find(:first, :conditions => [
-        "created_at < ? AND target = ? AND testtype = ? AND hwproduct = ? AND published = ? AND release_version = ?", time, target, testtype, hwproduct, true, release_version
+        "tested_at < ? AND target = ? AND testtype = ? AND hwproduct = ? AND published = ? AND release_version = ?", time, target.downcase, testtype.downcase, hwproduct.downcase, true, release_version
       ],
-      :order => "created_at DESC")
+      :order => "tested_at DESC")
   end
   
   def next_session
     MeegoTestSession.find(:first, :conditions => [
-        "created_at > ? AND target = ? AND testtype = ? AND hwproduct = ? AND published = ? AND release_version = ?", created_at, target, testtype, hwproduct, true, release_version
+        "tested_at > ? AND target = ? AND testtype = ? AND hwproduct = ? AND published = ? AND release_version = ?", created_at, target.downcase, testtype.downcase, hwproduct.downcase, true, release_version
       ],
-      :order => "created_at ASC")
+      :order => "tested_at ASC")
   end
   
   ###############################################
