34e7352eb708b6ed07f248ba9ad0ec1587aa8345
Janne Hietamaki <janne.hietamaki@leonidasoy.fi>
2010-12-13 16:30:23 +0200

    test for upload of multiple reports via REST API

--- features/api.feature
+++ features/api.feature
@@ -3,23 +3,31 @@ Feature: REST API
   I want to upload reports via REST API
   So that they can be browsed by users
 
-  Scenario: Uploading test report with HTTP POST
+  Background:
     Given I am an user with a REST authentication token
-    And the client sends file "sim.xml" via REST API
+
+  Scenario: Uploading test report with HTTP POST
+    When the client sends file "sim.xml" via the REST API
 
     Then the REST result "ok" is "1"
     And I should be able to view the created report
 
+  Scenario: Uploading test report with multiple files and attachments
+    When the client sends file with attachments via the REST API
+    Then the REST result "ok" is "1"
+    And I should be able to view the created report
+
+    Then I should see "SIM" within ".category_name"
+    And I should see "BT" within ".category_name"
+    
   Scenario: Sending REST import without valid report file
-    Given I am an user with a REST authentication token
-    And the client sends a request without file via REST API
+    When the client sends a request without file via the REST API
 
     Then the REST result "ok" is "0"
     Then the REST result "errors|uploaded_files" is "can't be blank"
 
   Scenario: Sending REST import without valid parameters
-    Given I am an user with a REST authentication token
-    And the client sends a request without parameter "target" via REST API
+    When the client sends a request without parameter "target" via the REST API
 
     Then the REST result "ok" is "0"
     Then the REST result "errors|target" is "can't be blank"
--- features/step_definitions/api_steps.rb
+++ features/step_definitions/api_steps.rb
@@ -2,7 +2,7 @@ Given /^I am an user with a REST authentication token$/ do
   Given %{I have one user "John Restless" with email "resting@man.net" and password "secretpass" and token "foobar"}
 end
 
-When /^the client sends file "([^"]*)" via REST API$/ do |file|
+When /^the client sends file "([^"]*)" via the REST API$/ do |file|
   post "/api/import?auth_token=foobar", {
       "report"          => Rack::Test::UploadedFile.new("features/resources/#{file}", "text/xml"),
       "release_version" => "1.2",
@@ -13,7 +13,22 @@ When /^the client sends file "([^"]*)" via REST API$/ do |file|
   response.should be_success
 end
 
-When /^the client sends a request without file via REST API$/ do
+When /^the client sends file with attachments via the REST API$/ do
+  post "/api/import?auth_token=foobar", {
+      "report.1"        => Rack::Test::UploadedFile.new("features/resources/sim.xml", "text/xml"),
+      "report.2"        => Rack::Test::UploadedFile.new("features/resources/bluetooth.xml", "text/xml"),
+      "attachment.1"    => Rack::Test::UploadedFile.new("public/images/ajax-loader.gif", "image/gif"),
+      "attachment.2"    => Rack::Test::UploadedFile.new("public/images/icon_alert.gif", "image/gif"),
+      "release_version" => "1.2",
+      "target"          => "Core",
+      "testtype"        => "automated",
+      "hwproduct"       => "N900"
+  }
+  response.should be_success
+end
+
+
+When /^the client sends a request without file via the REST API$/ do
   post "/api/import?auth_token=foobar", {
       "release_version" => "1.2",
       "target"          => "Core",
@@ -23,12 +38,9 @@ When /^the client sends a request without file via REST API$/ do
   }
 end
 
-When /^the client sends a request without parameter "target" via REST API$/ do
+When /^the client sends a request without parameter "target" via the REST API$/ do
   post "/api/import?auth_token=foobar", {
-      "report.1"     => Rack::Test::UploadedFile.new("features/resources/sim.xml", "text/xml"),
-      "report.2"     => Rack::Test::UploadedFile.new("features/resources/bluetooth.xml", "text/xml"),
-      "attachment.1" => Rack::Test::UploadedFile.new("features/resources/sim.xml", "text/xml"),
-      "attachment.2" => Rack::Test::UploadedFile.new("features/resources/bluetooth.xml", "text/xml"),
+      "report"     => Rack::Test::UploadedFile.new("features/resources/sim.xml", "text/xml"),
       "testtype"     => "automated",
       "hwproduct"    => "N900"
   }
